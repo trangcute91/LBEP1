bf5d188426584d0a7ebdc116d9683e8a4e39eca6
trangcute91 <tomomo295@gmail.xn--cm-8ja>
2021-05-20 16:10:56 +0700

    Update Bai 4.cpp

--- Assigment 4/Bai 4.cpp
+++ Assigment 4/Bai 4.cpp
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main(){
 	float a, b, c;
-	printf("Nhap canh a =");
+	printf("Nhap 3 canh de tao thanh 1 tam giac");
+	while(a+b<=c || a+c<=b || b+c<=a){
+	printf("\nNhap canh a =");
 	scanf("%f",&a);
-	printf("Nhap canh b =");
+	
+	printf("\nNhap canh b =");
 	scanf("%f",&b);
-	printf("Nhap canh c =");
+	
+	printf("\nNhap canh c =");
 	scanf("%f",&c); 
-	while(a+b>c && a+c>b && b+c>a){
+	if(a+b<=c || a+c<=b || b+c<=a){
+		printf("\n3 so vua nhap khong phai la 1 tam giac, xin nhap lai 3 so khac");
+		}
+		}
 	float d,p,s;
 	d = a+b+c;
 	p = d/2;
 	s = sqrt(p*(p-a)*(p-b)*(p-c));
-	printf("Chu vi cua tam giac = %f, Dien tich cua tam giac =%f",d,s );break;
-	}
+	printf("\nChu vi cua tam giac = %f",d );
+	printf("\nDien tich cua tam giac =%f",s );
 	}
